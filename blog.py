4ff21d76fef52a9005f48ae416cd19e01f90e6da
dengjonathan <dengjonathan@gmail.com>
2016-05-08 10:15:58 -0400

    new signup page

--- blog.py
+++ blog.py
@@ -1,12 +1,16 @@
 import os
 import re
-from string import letters
+import string
 import hmac
+import hashlib
+import random
+# import bcrypt
 
 import webapp2
 import jinja2
 
 from google.appengine.ext import db
+from webapp2_extras import sessions
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
@@ -14,6 +18,21 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
 
 with open('secrets.txt') as secret:
     CLIENT_SECRET = secret.read().strip()
+
+
+# valid signin functions
+def valid_username(username):
+    """returns True if username entered and is valid"""
+    user_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
+    return username and user_RE.match(username)
+
+def valid_pass(password):
+    pass_RE = re.compile("^.{3,20}$")
+    return password and pass_RE.match(password)
+
+def valid_email(email):
+    email_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+    return email_RE.match(email)
 
 # hash functions
 def hash_str(s):
@@ -37,6 +56,34 @@ def check_secure_value(h):
     else:
         return None
 
+# Todo add bcrypt module
+
+def make_salt(n):
+    """returns string of n random characters for use in hashing"""
+    choices = string.ascii_letters + string.digits
+    salt = ''.join(choices[random.randint(0, len(choices))] for i in range(n))
+    return salt
+
+def make_pw_hash(name, pw, salt=None):
+    """
+    returns hash of name and password using salt
+    of five random alpanumeric chars
+    """
+    if not salt:
+        salt = make_salt(5)
+    hash = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (hash, salt)
+
+def valid_password(name, pw, hash):
+    hash = hash.split(',')
+    salt = hash[1]
+    if make_pw_hash(name, pw, salt=salt) == hash:
+        return True
+    else:
+        return False
+
+
+# webapp2 Request Handlers
 class BaseHandler(webapp2.RequestHandler):
 
     def render_str(self, template, **params):
@@ -48,6 +95,21 @@ class BaseHandler(webapp2.RequestHandler):
 
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
+
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
 
 
 class MainPage(BaseHandler):
@@ -89,9 +151,35 @@ class MainPage(BaseHandler):
 class Welcome(BaseHandler):
 
     def get(self):
-        title = self.request.get('title')
-        message = self.request.get('message')
-        self.render('welcome.html', title=title, message=message)
+        username = self.session['username']
+        self.render('welcome.html', username=username)
+
+class Signup(BaseHandler):
+
+    def get(self):
+        self.render('signup.html', params=None, error=None)
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        password_check = self.request.get('password_check')
+        email = self.request.get('email')
+        error = {}
+        params = {'username': username, 'email':email}
+
+        if not valid_username(username):
+            error['username'] = 'Enter valid username between 3 and 20 characters.'
+        if not valid_pass(password):
+            error['password'] = 'Enter valid username between 3 and 20 characters.'
+        if not password == password_check:
+            error['password_check'] = 'Passwords must match'
+        if email:
+            if not valid_email(email):
+                error['email'] = 'Enter valid email address'
+        if error:
+            self.render('signup.html', error=error, params=params)
+        else:
+            self.redirect('/welcome')
 
 # database classes
 
@@ -100,7 +188,13 @@ class Post(db.Model):
     message = db.TextProperty()
     created_at = db.DateTimeProperty(auto_now_add=True)
 
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': CLIENT_SECRET,
+}
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/welcome', Welcome)],
+                               ('/welcome', Welcome),
+                               ('/signup', Signup)],
+                              config=config,
                               debug=True)
